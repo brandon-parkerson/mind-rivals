dafb0a525f82cde1dd6d7b159670e945f2eea465
Brandon Parkerson <bpkp1996@Osmo.>
2025-11-13 13:30:25 +0200

    init commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import phaser from "phaser";
+document.querySelector("#app").innerHTML = `
+  <div>
+   <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      test test
+    </p>
+  </div>
+`;
